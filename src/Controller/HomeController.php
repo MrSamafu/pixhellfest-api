90b7e0eb6147d71cf653b54c916f14dbc48c5dc4
Samson <51507673+MrSamafu@users.noreply.github.com>
2024-06-03 14:18:19 +0200

    add Form + update createAccount

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use PhpParser\Node\Expr\Cast\Object_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,7 +21,20 @@ class HomeController extends AbstractController
     #[Route('/createAccount', name: 'createAccount', methods: ['GET','POST'])]
     public function createAccount(Request $request): Response
     {
+
         if($request->isMethod('POST')) {
+
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $repassword = $_POST['repassword'];
+            $userRepo = New UserRepository();
+
+            if($password == $repassword){
+                if(count($userRepo->findBy(['username' => $username])) < 1){
+                    dd('gg');
+                }
+            }
             return $this->render('home/index.html.twig');
         }
         elseif($request->isMethod('GET')) {
